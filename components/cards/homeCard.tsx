a91e5bf6f19cd03c7b5b38e3f4a2e5d0c50d7ec5
vitolinho <93949338+vitolinho@users.noreply.github.com>
2023-09-23 22:44:32 +0200

    💄 add HomeCard

---
+++ components/cards/homeCard.tsx
@@ -0,0 +1,33 @@
+"use client"
+
+import Button from "@/components/buttons/button"
+import { RiPlayFill } from "react-icons/ri"
+import { RiInformationFill } from "react-icons/ri"
+
+interface HomeCardPorperties {
+  backgroundImage: string,
+  title: string,
+  info: string,
+}
+
+// eslint-disable-next-line no-unused-vars
+const HomeCard = ({ backgroundImage, title, info } :HomeCardPorperties) => {
+  const cardStyle = {
+    background: `url(${backgroundImage}) no-repeat center center`,
+    backgroundSize: "cover"
+  }
+  return (
+    <div className="flex flex-col gap-y-3 h-[450px]" style={cardStyle}>
+      <div className="flex flex-col justify-center items-center mt-[300px]">
+        <p className="h4-hind-m">{title}</p>
+        <p className="button-sm">{info}</p>
+      </div>
+      <div className="px-7 py-3 flex flex-row justify-between">
+        <Button variant={"primary"} size={"md"} icon={<RiPlayFill/>} iconPosition="left">Lecture</Button>
+        <Button variant={"secondary"} size={"md"} icon={<RiInformationFill/>} iconPosition="left">Plus d&apos;infos</Button>
+      </div>
+    </div>
+  )
+}
+
+export default HomeCard
