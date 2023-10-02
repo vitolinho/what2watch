1b8b4efc9ae28b949a98e636557a35822db5c397
vitolinho <93949338+vitolinho@users.noreply.github.com>
2023-10-02 13:52:57 +0200

    🎨 update data

--- next.config.js
+++ next.config.js
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  images: {
+      dangerouslyAllowSVG: true,
+      domains: ['fr.web.img3.acsta.net', 'fr.web.img6.acsta.net', 'fr.web.img5.acsta.net', 'fr.web.img4.acsta.net', 'fr.web.img2.acsta.net'],
+      contentDispositionType: 'attachment',
+      contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
+  }
+}
 
 module.exports = nextConfig
