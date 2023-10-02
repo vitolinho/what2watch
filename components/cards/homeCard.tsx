b9789a352842690b7fb40841cea939b8e0bc2fe6
vitolinho <93949338+vitolinho@users.noreply.github.com>
2023-10-02 15:57:22 +0200

    💄 update homecard

--- components/cards/homeCard.tsx
+++ components/cards/homeCard.tsx
@@ -27,21 +27,21 @@ const HomeCard: React.FC<HomeCardPorperties> = ({ backgroundImage, title, descri
   }, [])
   return (
     <div className={isDesktop ? "hidden" : "px-5"}>
-      <div className="relative gap-y-3 h-fit rounded-md py-5 z-0">
+      <div className="relative gap-y-3 h-[600px] rounded-md py-5 z-0">
         <Image
           src={backgroundImage}
           alt="test"
           width={100}
           height={100}
           className="w-full h-full opacity-50 object-cover z-10 rounded-md"/>
-        <div className="absolute top-0 left-0 w-full h-fit mt-[200px] py-5">
+        <div className="absolute bottom-5 left-0 w-full h-fit py-5">
           <div className="relative z-10">
-            <div className="flex flex-col justify-center items-center px-2 z-20">
+            <div className="flex flex-col justify-center items-center px-2 z-20 gap-y-2">
               <p className="h4-hind-m">{title}</p>
               <p className="button-sm">{description}</p>
             </div>
           </div>
-          <div className="flex flex-col gap-y-4 px-2 z-20">
+          <div className="flex flex-col gap-y-4 px-2 z-20 mt-5">
             <Link href={`/films/${id}/#watch-movie`}>
               <Button variant={"primary"} size={"md"} icon={<RiPlayFill/>} iconPosition="left" className="w-full">Lecture</Button>
             </Link>
