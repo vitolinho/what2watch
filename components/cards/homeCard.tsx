ff6af80a6fbf4a8d2f25daaff993094e7450ab89
vitolinho <93949338+vitolinho@users.noreply.github.com>
2023-10-02 15:44:34 +0200

    💄 update homeCard

--- components/cards/homeCard.tsx
+++ components/cards/homeCard.tsx
@@ -1,5 +1,6 @@
 "use client"
 
+import Image from "next/image"
 import Button from "@/components/buttons/button"
 import Link from "next/link"
 import { useEffect, useState } from "react"
@@ -14,10 +15,6 @@ interface HomeCardPorperties {
 }
 
 const HomeCard: React.FC<HomeCardPorperties> = ({ backgroundImage, title, description, id }) => {
-  const cardStyle = {
-    background: `url(${backgroundImage}) no-repeat center center`,
-    backgroundSize: "cover"
-  }
   const [isDesktop, setIsDesktop] = useState(false)
 
   useEffect(() => {
@@ -30,18 +27,28 @@ const HomeCard: React.FC<HomeCardPorperties> = ({ backgroundImage, title, descri
   }, [])
   return (
     <div className={isDesktop ? "hidden" : "px-5"}>
-      <div className="flex flex-col gap-y-3 h-fit rounded-md py-5" style={cardStyle}>
-        <div className="flex flex-col justify-center items-center mt-[300px] px-2">
-          <p className="h4-hind-m">{title}</p>
-          <p className="button-sm">{description}</p>
-        </div>
-        <div className="flex flex-col gap-y-4 px-2">
-          <Link href={`/films/${id}/#watch-movie`}>
-            <Button variant={"primary"} size={"md"} icon={<RiPlayFill/>} iconPosition="left" className="w-full">Lecture</Button>
-          </Link>
-          <Link href={`/films/${id}`}>
-            <Button variant={"secondary"} size={"md"} icon={<RiInformationFill/>} iconPosition="left" className="w-full">Plus d&apos;infos</Button>
-          </Link>
+      <div className="relative gap-y-3 h-fit rounded-md py-5 z-0">
+        <Image
+          src={backgroundImage}
+          alt="test"
+          width={100}
+          height={100}
+          className="w-full h-full opacity-50 object-cover z-10 rounded-md"/>
+        <div className="absolute top-0 left-0 w-full h-fit mt-[200px] py-5">
+          <div className="relative z-10">
+            <div className="flex flex-col justify-center items-center px-2 z-20">
+              <p className="h4-hind-m">{title}</p>
+              <p className="button-sm">{description}</p>
+            </div>
+          </div>
+          <div className="flex flex-col gap-y-4 px-2 z-20">
+            <Link href={`/films/${id}/#watch-movie`}>
+              <Button variant={"primary"} size={"md"} icon={<RiPlayFill/>} iconPosition="left" className="w-full">Lecture</Button>
+            </Link>
+            <Link href={`/films/${id}`}>
+              <Button variant={"secondary"} size={"md"} icon={<RiInformationFill/>} iconPosition="left" className="w-full">Plus d&apos;infos</Button>
+            </Link>
+          </div>
         </div>
       </div>
     </div>
